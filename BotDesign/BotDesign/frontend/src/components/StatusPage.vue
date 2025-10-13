bfbfad4bfe8ebf6a8458ae1fe8c128566f60b15e
Little-pure-light <purelighthappy@gmail.com>
2025-10-13 08:28:00 +0800

    Create StatusPage.vue

---
+++ BotDesign/BotDesign/frontend/src/components/StatusPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <HealthStatus />
+  </div>
+</template>
+
+<script>
+import HealthStatus from "../components/HealthStatus.vue";
+
+export default {
+  components: {
+    HealthStatus,
+  },
+};
+</script>
