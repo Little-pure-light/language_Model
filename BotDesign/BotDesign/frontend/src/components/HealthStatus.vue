b3c62e744406bb01d93241d8f4ae91c4303a69d5
Little-pure-light <purelighthappy@gmail.com>
2025-10-13 08:26:56 +0800

    Create HealthStatus.vue

---
+++ BotDesign/BotDesign/frontend/src/components/HealthStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>🚦 系統健康狀態檢查</h2>
+    <pre>{{ healthData }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      healthData: {},
+    };
+  },
+  mounted() {
+    fetch("/api/health")
+      .then((res) => res.json())
+      .then((data) => {
+        this.healthData = data;
+      })
+      .catch((err) => {
+        this.healthData = { error: "無法取得健康檢查資料", detail: err.message };
+      });
+  },
+};
+</script>
