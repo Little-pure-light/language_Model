77d2fedac17a8015c3d1f3bf85a1e903625574e1
Little-pure-light <purelighthappy@gmail.com>
2025-10-13 10:28:38 +0800

    Update main.js

--- BotDesign/BotDesign/frontend/src/main.js
+++ BotDesign/BotDesign/frontend/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
 import router from './router'; // <-- 加進行
-
 const app = createApp(App);
-
-app.use(router); // <-- 加這行
+app.use(router);
 app.mount('#app');
